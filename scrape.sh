1b97677d5c58ddeba91c9e682b0a134bc434a8e2
Phil Mocek <phil@mocek.org>
2026-06-24 18:34:35 +0000

    2026-06-24T18:34:35+00:00 UTC

--- scrape.sh
+++ scrape.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
-# ./download.sh 'https://www.example.com/'
+
+# Flock Safety Transparency Portal - Git Scraper
+# Fetches agency list and individual agency transparency data
+# See https://simonwillison.net/2020/Oct/9/git-scraping/
+
+set -e
+
+# 1. Agency list from haveibeenflocked.com (works, no Cloudflare)
+./download.sh 'https://haveibeenflocked.com/news/transparency-portals/'
+
+# 2. Individual Washington agency transparency portals
+# Note: These are behind Cloudflare. curl may get blocked.
+# Uncomment and test individually once a bypass is working:
+#
+# WA_AGENCIES=(
+#   "piedmont-ca-pd"
+# )
+# for slug in "${WA_AGENCIES[@]}"; do
+#   ./download.sh "https://transparency.flocksafety.com/${slug}"
+# done
+
+# 3. Future: Piedmont CA as test case
+# ./download.sh 'https://transparency.flocksafety.com/piedmont-ca-pd'
